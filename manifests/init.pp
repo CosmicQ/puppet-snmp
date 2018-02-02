7275163f1598940b020638d74f28e61ab65e8484
CosmicQ <jason@cosmicegg.net>
2018-02-02 16:17:35 -0500

    Getting the config ready

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,19 @@
-class snmp {
+class snmp (
+
+  $rocommunity = 'public',
+  $allowed_ip  = '127.0.0.1',
+  $syscontact  = 'Sysadmin',
+  $syslocation = 'Somewhere',
+
+){
 
   if $facts['kernel'] == 'Linux' {
  	
     contain snmp::linux::install
+    contain snmp::linux::config
 
     Class['::snmp::linux::install']
+    -> Class['::snmp::linux::config']
 
   }
   elsif $facts['kernel'] == 'Windows' {
--- manifests/linux/config.pp
+++ manifests/linux/config.pp
@@ -1,4 +1,17 @@
 class snmp::linux::config {
 
+  $stuff = "rocommunity ${rocommunity} ${allowed_ip}
+        syscontact ${syscontact}
+        syslocation ${syslocation}
+        "
+
+  file { 'snmpd.conf':
+    ensure  => present,
+    path    => '/etc/snmp/snmpd.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => $stuff,
+  }
 
 }
