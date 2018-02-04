9fc4f7d6bc80146789086db65f18dc45f02a5afa
CosmicQ <jason@cosmicegg.net>
2018-02-04 16:52:52 -0500

    Fixing windows config - Had to escape \s

--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -1,34 +1,34 @@
 class snmp::windows::config {
 
   registry_key { [
-    'HKLM\SYSTEM\CurrentControlSet\services\SNMP\Parameters\PermittedManagers',
-    'HKLM\SYSTEM\CurrentControlSet\services\SNMP\Parameters\RFC1156Agent',
-    'HKLM\SYSTEM\CurrentControlSet\services\SNMP\Parameters\ValidCommunities' ]:
+    'HKLM\SYSTEM\CurrentControlSet\\services\SNMP\Parameters\PermittedManagers',
+    'HKLM\SYSTEM\CurrentControlSet\\services\SNMP\Parameters\RFC1156Agent',
+    'HKLM\SYSTEM\CurrentControlSet\\services\SNMP\Parameters\ValidCommunities' ]:
     purge_values => true,
   }
 
-  registry_value { "HKLM\SYSTEM\CurrentControlSet\services\SNMP\Parameters\ValidCommunities\\${snmp::community}":
+  registry_value { "HKLM\SYSTEM\CurrentControlSet\\services\SNMP\Parameters\ValidCommunities\\${snmp::community}":
     ensure => present,
     type   => 'dword',
     data   => '4',
   }
 
-  registry_value { "HKLM\SYSTEM\CurrentControlSet\services\SNMP\Parameters\PermittedManagers\1":
+  registry_value { "HKLM\SYSTEM\CurrentControlSet\\services\SNMP\Parameters\PermittedManagers\1":
     ensure => present,
     type   => 'string',
-    data   => ${snmp::allowed_ip},
+    data   => $snmp::allowed_ip,
   }
 
-  registry_value { "HKLM\SYSTEM\CurrentControlSet\services\SNMP\Parameters\RFC1156Agent\sysContact":
+  registry_value { "HKLM\SYSTEM\CurrentControlSet\\services\SNMP\Parameters\RFC1156Agent\sysContact":
     ensure => present,
     type   => 'string',
-    data   => ${snmp::syscontact},
+    data   => $snmp::syscontact,
   }
 
-  registry_value { "HKLM\SYSTEM\CurrentControlSet\services\SNMP\Parameters\RFC1156Agent\sysLocation":
+  registry_value { "HKLM\SYSTEM\CurrentControlSet\\services\SNMP\Parameters\RFC1156Agent\sysLocation":
     ensure => present,
     type   => 'string',
-    data   => ${snmp::syslocation},
+    data   => $snmp::syslocation,
   }
 
 }
